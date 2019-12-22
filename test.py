8478acc425692cc00b940c080bdeb6dd173404a9
Mark Fishel <fishel@ut.ee>
2019-12-22 22:50:33 +0200

    added generic "move 3 motors for time t" function

--- test.py
+++ test.py
@@ -4,20 +4,22 @@ import sys
 import time
 import ZeroBorg
 
-zb = ZeroBorg.ZeroBorg()
-zb.Init()
-zb.SetMotor1(1)
-zb.SetMotor2(1)
-zb.SetMotor3(1)
-zb.SetMotor4(1)
-time.sleep(0.3)
-zb.SetMotor1(-1)
-zb.SetMotor2(-1)
-zb.SetMotor3(-1)
-zb.SetMotor4(-1)
-time.sleep(0.3)
-zb.SetMotor1(0)
-zb.SetMotor2(0)
-zb.SetMotor3(0)
-zb.SetMotor4(0)
-print("Done!")
+class Zbg:
+	def __init__(self):
+		self.zb = ZeroBorg.ZeroBorg()
+		self.zb.Init()
+	
+	def stop(self):
+		self.zb.SetMotor1(0)
+		self.zb.SetMotor2(0)
+		self.zb.SetMotor3(0)
+		self.zb.SetMotor4(0)
+	
+	def movet(self, d1, d2, d3, t = 1):
+		zb.SetMotor1(d1)
+		zb.SetMotor3(d2)
+		zb.SetMotor4(d3)
+		
+		time.sleep(t)
+		
+		self.stop()
